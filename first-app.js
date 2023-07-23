33d31733b158ff33ad6dcfdd24c031d830b3de0a
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-07-23 16:47:29 +0530

    working with forms

--- first-app.js
+++ first-app.js
@@ -1,34 +1,23 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  url = req.url;
-  if (url === "/home") {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title>Home</title></head>");
-    res.write("<body><h1>welcome to home</h1></body>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message'method='POST'> <input type='text'  name='message' > <button type='submit' >send</button>   </form></body>"
+    );
     res.write("<html>");
     return res.end();
-  } else if (url === "/about") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>About</title></head>");
-    res.write("<body><h1>welcome to About Us page</h1></body>");
-    res.write("<html>");
-    return res.end();
-  } else if (url === "/node") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Node</title></head>");
-    res.write("<body><h1>welcome to Node Page</h1></body>");
-    res.write("<html>");
+  } else if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "dummy text");
+    res.statusCode = 302;
+    res.setHeader("location", "/");
     return res.end();
   }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first Page</title></head>");
-  res.write("<body><h1>hello from node js</h1></body>");
-  res.write("<html>");
-  res.end();
 });
 server.listen(4000);
